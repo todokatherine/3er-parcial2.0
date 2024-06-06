d40a76da9d1a59ce5d1fe09fe6cebac2962e19e4
Sanchez Morales Katherine <168496765+todokatherine@users.noreply.github.com>
2024-06-06 12:15:42 -0600

    Create programa17.cpp

---
+++ programa17.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Los números del 1 al 100 son:" << std::endl;
+    for (int i = 1; i <= 100; i++) {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
