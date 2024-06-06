83b145c9ddb78830f03466293f05495dd6380fa0
Sanchez Morales Katherine <168496765+todokatherine@users.noreply.github.com>
2024-06-06 11:45:58 -0600

    Create programa14.cpp

---
+++ programa14.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1,num2;
+
+cout<<"dime el primer numero";
+cin>>num1;
+cout<<"dime el segundo numero";
+cin>>num2;
+
+if(num1>num2){;
+cout<<"el numero mayor es"<<num1<<endl;
+}
+ else if(num1<num2){;
+cout<<"el numero menor es"<<num2<<endl;
+}
+else {;
+cout<<"los numeros son iguales";
+}
+return 0;
+}
