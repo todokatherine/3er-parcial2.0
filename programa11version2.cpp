8eb2c6b3dfe7b0d4e434362c150a7885f04bc15c
Sanchez Morales Katherine <168496765+todokatherine@users.noreply.github.com>
2024-06-06 11:33:48 -0600

    Update and rename programa11.cpp to programa11version2.cpp

--- programa11version2.cpp
+++ programa11version2.cpp
@@ -5,22 +5,22 @@ int main ()
 int hora;
 cout<<"dime la hora";
 cin>>hora;
-
 if (hora<12){;
-
 cout<<"buenos dias";
 }
-
 if(hora>=13 &&hora <=19){;
 hora<19;
-
+ 
 cout<<"buenas tardes";
-
+}
+if(hora>=20 &&hora <=24){;
+cout<<"buenas noches";
 }
 
-if(hora>=20 &&hora <=24){;
+if(hora>=25){;
 
-cout<<"buenas noches";
+cout<<"hora invalida";
+
 }
 
 return 0;
