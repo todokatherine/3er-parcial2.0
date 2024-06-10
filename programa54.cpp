e7247138752180b230f40f43bedc2db3d87e6843
Sanchez Morales Katherine <168496765+todokatherine@users.noreply.github.com>
2024-06-10 10:38:01 -0600

    Create programa54.cpp

---
+++ programa54.cpp
@@ -0,0 +1,7 @@
+// Inciso c:Katherine Verificar si el número es primo
+    bool primo = esPrimo(numero_entero);
+    if (primo) {
+        std::cout << "c) " << numero_entero << " es un número primo." << std::endl;
+    } else {
+        std::cout << "c) " << numero_entero << " no es un número primo." << std::endl;
+    }
